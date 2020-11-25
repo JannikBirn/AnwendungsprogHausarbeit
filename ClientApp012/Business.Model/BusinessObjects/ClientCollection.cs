ed8054dde627328b802a311037c86a43f68985a7
JannikBirn <58862960+JannikBirn@users.noreply.github.com>
2020-11-25 18:47:20 +0100

    initial commit

---
+++ ClientApp012/Business.Model/BusinessObjects/ClientCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp012.Business.Model.BusinessObjects
+{
+    public class ClientCollection : ObservableCollection<Client>
+    {
+        public ClientCollection()
+        {
+
+        }
+    }
+}
---
+++ ClientApp012/Logic.Ui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using De.HsFlensburg.ClientApp012.Logic.Ui.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp012.Logic.Ui.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ClientCollectionViewModel MyList { get; set; }
+        public RelayCommand AddClientToList { get; }
+
+        public MainWindowViewModel()
+        {
+            AddClientToList = new RelayCommand(() => AddClientToListMethod());
+            MyList = new ClientCollectionViewModel();
+        }
+
+        private void AddClientToListMethod()
+        {
+            ClientViewModel clientVM = new ClientViewModel();
+            //clientVM.Id = Int16.Parse(IdTextBox.Text);
+            //clientVM.Name = NameTextBox.Text;
+            MyList.Add(clientVM);
+            
+        }
+
+
+
+        //private void DelCientInList(object sender, RoutedEventArgs e)
+        //{
+        //    ClientCollectionViewModel list = this.FindResource("myList") as ClientCollectionViewModel;
+        //    if (list != null)
+        //    {
+        //        list.RemoveAt(0);
+        //    }
+        //}
+
+
+
+        //private void DelCollection(object sender, RoutedEventArgs e)
+        //{
+        //    ClientCollectionViewModel list = this.FindResource("myList") as ClientCollectionViewModel;
+        //    if (list != null)
+        //    {
+        //        list.Clear();
+        //    }
+        //}
+    }
+}
