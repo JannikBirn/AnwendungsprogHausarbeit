da415588d5f5c71c34eb95619c906cee90eb6237
Jan <jahi2064@stud.hs-flensburg.de>
2021-01-20 17:40:12 +0100

    added PrintService

---
+++ ClientApp012/PrintingService/PrintSingleCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Printing;
+
+
+namespace PrintingService
+{
+    class PrintSingleCard
+    {
+        public PrintSingleCard()
+        {
+
+        }
+        public void TestPrint()
+        {
+
+        }
+        public void TestPrintWithDialogue()
+        {
+
+        }
+
+
+    }
+}
