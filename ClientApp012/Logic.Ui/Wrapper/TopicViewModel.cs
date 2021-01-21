c07a070ec6edbaed2ec2d5a1b063919fa47824c4
JannikBirn <58862960+JannikBirn@users.noreply.github.com>
2021-01-21 13:24:08 +0100

    updated Wrapper classes

--- ClientApp012/Logic.Ui/Wrapper/TopicViewModel.cs
+++ ClientApp012/Logic.Ui/Wrapper/TopicViewModel.cs
@@ -9,24 +9,25 @@ namespace De.HsFlensburg.ClientApp012.Logic.Ui.Wrapper
 {
     public class TopicViewModel : ViewModelSyncCollection<CardViewModel, Card, Topic>
     {
-        private CardViewModel cardViewModel;
+        //private CardViewModel cardViewModel;
 
-        public CardViewModel CardViewModel
-        {
-            get
-            {
-                return cardViewModel;
-            }
-            set
-            {
-                cardViewModel = value;
-                OnPropertyChanged("CardViewModel");
-            }
-        }
+        //public CardViewModel CardViewModel
+        //{
+        //    get
+        //    {
+        //        return cardViewModel;
+        //    }
+        //    set
+        //    {
+        //        cardViewModel = value;
+        //        OnPropertyChanged("CardViewModel");
+        //    }
+        //}
 
         public TopicViewModel() : base()
         {
-            CardViewModel = new CardViewModel();
+            //CardViewModel = new CardViewModel();
+            //this.Model[0] = CardViewModel.Model;
 
             //this.Model.Name = 
             //this.Model.Img
