90d6aa77060e7d9ddac50b108da9e29b46583816
Jannli <jannliyt@gmail.com>
2021-02-01 21:33:23 +0100

    fixed class name

--- ClientApp012/Logic.Ui/ViewModels/MainWindowViewModel.cs
+++ ClientApp012/Logic.Ui/ViewModels/MainWindowViewModel.cs
@@ -66,7 +66,7 @@ namespace De.HsFlensburg.ClientApp012.Logic.Ui.ViewModels
 
         private void OpenCardOverViewMethod()
         {
-            ServiceBus.Instance.Send(new OpenNewCardOverview());
+            ServiceBus.Instance.Send(new OpenNewCardOverViewMessage());
         }
 
         //private void DelCientInList(object sender, RoutedEventArgs e)
