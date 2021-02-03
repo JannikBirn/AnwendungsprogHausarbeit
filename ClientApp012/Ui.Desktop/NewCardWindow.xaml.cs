06c8a5588282b38ba3aa9eb2010bcdbbf6791213
L3nn3y <75038042+L3nn3y@users.noreply.github.com>
2021-02-03 20:02:42 +0100

    New Card Window Updated / VideoInspectWindow deleted
    VideoInspectWindow was unnecessary

--- ClientApp012/Ui.Desktop/NewCardWindow.xaml.cs
+++ ClientApp012/Ui.Desktop/NewCardWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace De.HsFlensburg.ClientApp012.Ui.Desktop
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
