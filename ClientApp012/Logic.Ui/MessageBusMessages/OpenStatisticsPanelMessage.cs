5e2b97790084f77cee533bb100d57fa70338bd09
Jannli <jannliyt@gmail.com>
2021-02-03 14:55:24 +0100

    Added new Msg Bus with parameter

---
+++ ClientApp012/Logic.Ui/MessageBusMessages/OpenStatisticsPanelMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp012.Logic.Ui.MessageBusMessages
+{
+    public class OpenStatisticsPanelMessage
+    {
+        public string Message { get; set; }
+    }
+}
