b0e1546ed1d877d8a35f3f075a95bfaf6694eac7
Jannik Birn <jannliyt@gmail.com>
2021-02-05 16:37:47 +0100

    Added ItemsControlto LineGraph and VM

--- ClientApp012/Logic.Ui/ViewModels/LineGraphViewModel.cs
+++ ClientApp012/Logic.Ui/ViewModels/LineGraphViewModel.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace De.HsFlensburg.ClientApp012.Logic.Ui.ViewModels
 {
-    public class LineGraphViewModel
+    public class LineGraphViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<string> horizontalNumbers;
+        public ObservableCollection<string> HorizontalNumbers
+        {
+            get { return horizontalNumbers; }
+            set
+            {
+                horizontalNumbers = value;
+                OnPropertyChanged("HorizontalNumbers");
+            }
+        }
 
+
+        public LineGraphViewModel()
+        {
+            HorizontalNumbers = new ObservableCollection<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                HorizontalNumbers.Add("" + i);
+            }
+        }
+
+        //For the INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
